701059707a97bd552993b7c459fc42c4da135d3c
Terrance Li <terranceli.terbear@gmail.com>
2019-02-20 19:54:03 -0800

    Implemented local area type-S FP generation

--- controllers.js
+++ controllers.js
@@ -236,7 +236,28 @@ let Center = () => {
 				neighborStationsInArea,
 				neighborStationsOutsideArea,
 				stationsInArea: area.stations,
-				generateTypeSFlightPlan: function() {}
+				generateTypeSFlightPlan: function(dest) {
+					// this function is called when the destination of the inspected drone is within this area
+					let destStation = this.stationsInArea.filter(s =>
+						s.sector.isInside(dest)
+					)[0]
+
+					// convert stations into a simpler graph to work with
+					let nodes = this.stationsInArea.map(s => ({
+						id: s.id,
+						edges: s.neighborStationsInArea.map(neighbor => ({
+							id: neighbor.id,
+							cost: dist(s.sector.c, neighbor.sector.c)
+						}))
+					}))
+
+					let res = dijkstra(nodes, this.id, destStation.id)
+
+					return {
+						path: res.path,
+						terminator: `LOCAL`
+					}
+				}
 			}
 
 			area.stations.push(newStation) // add this station to the area
