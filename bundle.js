d3c9c1adca3bfbc31cd7906aa25e7a45ed8f05de
Terrance Li <terranceli.terbear@gmail.com>
2019-02-21 22:14:57 -0800

    Fixed multiple canvases rendering, finished browserify refactor

--- bundle.js
+++ bundle.js
@@ -82892,58 +82892,62 @@ module.exports = class Station {
 
 },{}],6:[function(require,module,exports){
 let p5 = require('p5')
+let center
 
-module.exports = center => {
-	return new p5(function() {
-		this.setup = function() {
-			console.log('SETUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP')
-			this.createCanvas(windowWidth, windowHeight)
-			this.background(32)
+let s = sketch => {
+	sketch.setup = function() {
+		// console.log(window)
+		sketch.createCanvas(window.innerWidth, window.innerHeight)
+		sketch.background(32)
 
-			this.textSize(16)
-			this.textAlign(CENTER)
+		sketch.textSize(16)
+		sketch.textAlign(sketch.CENTER)
 
-			this.frameRate(1)
-		}
+		sketch.frameRate(1)
+	}
 
-		this.draw = function draw() {
-			// clears the canvas
-			this.background(32)
+	sketch.draw = function() {
+		// clears the canvas
+		sketch.background(32)
 
-			center.areas.forEach(a => {
-				a.stations.forEach(s => {
-					let c = s.sector.c
-					let r = s.sector.r
-					let chords = s.sector.chords
+		center.areas.forEach(a => {
+			a.stations.forEach(s => {
+				let c = s.sector.c
+				let r = s.sector.r
+				let chords = s.sector.chords
 
-					this.noFill()
-					this.strokeWeight(4)
-					this.stroke('#ffffffaa')
+				sketch.noFill()
+				sketch.strokeWeight(4)
+				sketch.stroke('#ffffffaa')
 
-					if (s.sector.chords.length === 0) {
-						this.ellipse(c.x, c.y, r * 2, r * 2)
-					} else {
-						// TODO: render chords and arcs (yeah its hard)
-						// arc(c.x, c.y, r, r, 0, 2 * Math.PI)
-						this.ellipse(c.x, c.y, r * 2, r * 2)
+				if (s.sector.chords.length === 0) {
+					sketch.ellipse(c.x, c.y, r * 2, r * 2)
+				} else {
+					// TODO: render chords and arcs (yeah its hard)
+					// arc(c.x, c.y, r, r, 0, 2 * Math.PI)
+					sketch.ellipse(c.x, c.y, r * 2, r * 2)
 
-						chords.forEach(chord => {
-							let x1 = r * Math.cos(chord.t1) + c.x
-							let y1 = r * Math.sin(chord.t1) + c.y
-							let x2 = r * Math.cos(chord.t2) + c.x
-							let y2 = r * Math.sin(chord.t2) + c.y
+					chords.forEach(chord => {
+						let x1 = r * Math.cos(chord.t1) + c.x
+						let y1 = r * Math.sin(chord.t1) + c.y
+						let x2 = r * Math.cos(chord.t2) + c.x
+						let y2 = r * Math.sin(chord.t2) + c.y
 
-							this.line(x1, y1, x2, y2)
-						})
-					}
+						sketch.line(x1, y1, x2, y2)
+					})
+				}
 
-					this.noStroke()
-					this.fill('#ffffff')
-					this.text(`S${s.id}`, c.x, c.y)
-				})
+				sketch.noStroke()
+				sketch.fill('#ffffff')
+				sketch.text(`S${s.id}`, c.x, c.y)
 			})
-		}
-	})
+		})
+	}
+}
+
+module.exports = c => {
+	center = c
+	return new p5(s)
 }
 
 },{"p5":1}],7:[function(require,module,exports){
