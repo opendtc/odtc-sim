7267f3b01abe60f6a89e591549faed12561c1af7
Terrance Li <terranceli.terbear@gmail.com>
2019-02-21 21:01:52 -0800

    Rendering mostly functional with browserify; rendering multiple canvases for some reason

--- src/render.js
+++ src/render.js
@@ -1,47 +1,54 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight)
-	background(32)
+let p5 = require('p5')
 
-	textSize(16)
-	textAlign(CENTER)
+module.exports = center => {
+	return new p5(function() {
+		this.setup = function() {
+			console.log('SETUPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP')
+			this.createCanvas(windowWidth, windowHeight)
+			this.background(32)
 
-	frameRate(1)
-}
+			this.textSize(16)
+			this.textAlign(CENTER)
 
-function draw() {
-	// clears the canvas
-	background(32)
+			this.frameRate(1)
+		}
 
-	center.areas.forEach(a => {
-		a.stations.forEach(s => {
-			let c = s.sector.c
-			let r = s.sector.r
-			let chords = s.sector.chords
+		this.draw = function draw() {
+			// clears the canvas
+			this.background(32)
 
-			noFill()
-			strokeWeight(4)
-			stroke('#ffffffaa')
+			center.areas.forEach(a => {
+				a.stations.forEach(s => {
+					let c = s.sector.c
+					let r = s.sector.r
+					let chords = s.sector.chords
 
-			if (s.sector.chords.length === 0) {
-				ellipse(c.x, c.y, r * 2, r * 2)
-			} else {
-				// TODO: render chords and arcs (yeah its hard)
-				// arc(c.x, c.y, r, r, 0, 2 * Math.PI)
-				ellipse(c.x, c.y, r * 2, r * 2)
+					this.noFill()
+					this.strokeWeight(4)
+					this.stroke('#ffffffaa')
 
-				chords.forEach(chord => {
-					let x1 = r * Math.cos(chord.t1) + c.x
-					let y1 = r * Math.sin(chord.t1) + c.y
-					let x2 = r * Math.cos(chord.t2) + c.x
-					let y2 = r * Math.sin(chord.t2) + c.y
+					if (s.sector.chords.length === 0) {
+						this.ellipse(c.x, c.y, r * 2, r * 2)
+					} else {
+						// TODO: render chords and arcs (yeah its hard)
+						// arc(c.x, c.y, r, r, 0, 2 * Math.PI)
+						this.ellipse(c.x, c.y, r * 2, r * 2)
 
-					line(x1, y1, x2, y2)
+						chords.forEach(chord => {
+							let x1 = r * Math.cos(chord.t1) + c.x
+							let y1 = r * Math.sin(chord.t1) + c.y
+							let x2 = r * Math.cos(chord.t2) + c.x
+							let y2 = r * Math.sin(chord.t2) + c.y
+
+							this.line(x1, y1, x2, y2)
+						})
+					}
+
+					this.noStroke()
+					this.fill('#ffffff')
+					this.text(`S${s.id}`, c.x, c.y)
 				})
-			}
-
-			noStroke()
-			fill('#ffffff')
-			text(`S${s.id}`, c.x, c.y)
-		})
+			})
+		}
 	})
 }
--- src/sim.js
+++ src/sim.js
@@ -3,6 +3,7 @@ let Center = require('./Center')
 // initialize the central service
 let center = new Center()
 
+require('./render')(center)
 ;(async () => {
 	center.newStation({ x: 500, y: 300 }, 200, 0)
 	center.newStation({ x: 500, y: 600 }, 200, 0)
